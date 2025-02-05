6f0c9c1c9d2145af334d163696ba87e0a2055bf3
vishu512 <70514375+vishu512@users.noreply.github.com>
2025-02-06 00:32:49 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
-fetch("https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd")
+fetch("https://api.coingecko.com/api/v3/simple/price?ids=ethereum,bitcoin&vs_currencies=usd")
     .then(response => response.json())
     .then(data => {
+        console.log("BTC Price: $" + data.bitcoin.usd);
         console.log("ETH Price: $" + data.ethereum.usd);
     })
     .catch(error => console.error("Error fetching data", error));
